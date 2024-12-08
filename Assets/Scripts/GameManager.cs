ada635085ca413911de3925907b6ad41e4f70ebb
Manya Phutela <mphutela@umass.edu>
2024-12-07 22:13:50 -0500

    added plant assets

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
